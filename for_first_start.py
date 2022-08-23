ba955b9f53ecb956285d7f04e75969a49ca2831c
Daniil Stepanov <43065450+sweetlhare@users.noreply.github.com>
2022-08-23 22:30:09 +0300

    Update for_first_start.py

--- for_first_start.py
+++ for_first_start.py
@@ -3,25 +3,25 @@ import streamlit as st
 
 from deta import Deta
 
-deta_key = "your deta drive key"
+deta_key = 'your deta key'
 deta = Deta(deta_key)
-deta_drive = deta.Drive("hahastart_object_detection")
+deta_drive = deta.Drive("name of drive")
 
 
 registration_columns = [
     'Login',
     'Password',
-    'Email'
-    'Telegram'
-    'FIO'
+    'Email',
+    'Telegram',
+    'FIO',
 ]
 
-users_database = pd.DataFrame(columns=registration_columns).set_index('Login')
-users_database.to_csv('assets/users_database.csv')
+users_database = pd.DataFrame(columns=registration_columns)
+users_database.to_csv('assets/users_database.csv', index=False)
 deta_drive.put('assets/users_database.csv', path=f"./assets/users_database.csv")
 
 rating = pd.DataFrame(columns=['Login', 'Metric'])
-rating.to_csv('assets/rating.csv')
+rating.to_csv('assets/rating.csv', index=False)
 deta_drive.put('assets/rating.csv', path=f"./assets/rating.csv")
 
 deta_drive.put('assets/gt.csv', path=f"./assets/gt.csv")
