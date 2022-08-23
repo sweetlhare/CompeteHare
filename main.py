3c48a0b6fb6e27844096e5f19a5978b04ba7a655
Daniil Stepanov <43065450+sweetlhare@users.noreply.github.com>
2022-08-23 20:05:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,40 +4,62 @@ import pandas as pd
 import numpy as np
 
 
+from deta import Deta
+
+deta_key = st.secrets['deta_key']
+deta = Deta(deta_key)
+deta_drive = deta.Drive("hahastart_object_detection")
+
+
+from metric import calculate_metric
+
+
 # <------------------------------------------------------------------------->
 
 # st.set_page_config(layout="wide")
 
 registration_columns = [
-    'login',
-    'password',
-    'email'
-    # 'FIO',
-    # 'age'
+    'Login',
+    'Password',
+    'Email'
+    'Telegram'
+    'FIO'
 ]
 
-def load_users_database(path_to_db='assets/users_database.csv'):
-    if os.path.exists(path_to_db):
-        users_database = pd.read_csv(path_to_db)
-        users_database['login'] = users_database['login'].astype(str)
-        users_database['password'] = users_database['password'].astype(str)
-        users_database = users_database.set_index('login')
-    else:
-        users_database = pd.DataFrame(columns=registration_columns).set_index('login')
-        users_database.to_csv(path_to_db)
+def load_users(path_to_file='assets/users_database.csv'):
+    users_database = deta_drive.get(path_to_file)
     return users_database
 
-def update_users_database(login, user_values, path_to_db='assets/users_database.csv'):
+def update_users(login, user_values, path_to_file='assets/users_database.csv'):
     users_database.loc[str(login), registration_columns[1:]] = user_values
     users_database['password'] = users_database['password'].astype(str)
-    users_database.to_csv(path_to_db)
+    users_database.to_csv(path_to_file)
+    deta_drive.put(path_to_file, path=f"./{path_to_file}")
     return users_database
 
-users_database = load_users_database()
+def load_gt(path_to_file='assets/gt.csv'):
+    gt_file = deta_drive.get(path_to_file)
+    return gt_file
+
+def load_rating(path_to_file='assets/rating.csv'):
+    rating_file = deta_drive.get(path_to_file)
+    return rating_file
+
+def update_rating(login, metric, path_to_file='assets/rating.csv'):
+    rating.loc[rating.shape[0], 'Login'] = login
+    rating.loc[rating.shape[0], 'Metric'] = metric
+    rating.to_csv(path_to_file)
+    deta_drive.put(path_to_file, path=f"./{path_to_file}")
+    return rating
+
+users_database = load_users()
+gt = load_gt()
+rating = load_rating()
+
     
 # <------------------------------------------------------------------------->   
 
-# Using object notation
+# Sidebar Menu
 with st.sidebar:
     st.title('ХаХаСтарт: Object Detection')
     add_selectbox = st.radio(
@@ -45,14 +67,16 @@ with st.sidebar:
         ("Информация о задаче", "Рейтинг", "Регистрация", "Загрузка решения")
     )
 
+# Task Description
 if add_selectbox == 'Информация о задаче':
     st.header(add_selectbox)
 
-
+# Leaderboard
 if add_selectbox == 'Рейтинг':
     st.header(add_selectbox)
+    st.dataframe(rating)
 
-
+# New User Registration
 if add_selectbox == 'Регистрация':
     st.header(add_selectbox)
 
@@ -64,24 +88,26 @@ if add_selectbox == 'Регистрация':
 
     login = st.text_input('Логин *')
     password = st.text_input('Пароль *')
+    fio = st.text_input('ФИО *')
     email = st.text_input('Email *')
+    telegram = st.text_input('Telegram')
 
     registration_button = st.button('Зарегистрироваться')
 
     if registration_button:
         if login and password and email:
-            if users_database.loc[login, ].shape[0] > 0 and users_database.loc[login, 'password'] != password:
+            if login in users_database.index.values and users_database.loc[login, 'Password'] != password:
                 st.text('Пользователь с таким логином уже существует.')
                 st.text('''
                 Если вы забыли пароль, напишите запрос на восстановление
                 на почту ХХХ с почты, на которую зарегистрирован аккаунт.''')
             else:
-                users_database = update_users_database(login, [password, email])
+                users_database = update_users(login, [password, email, telegram, fio])
                 st.text('Вы успешно зарегистрировались! Можно загружать решения.')
         else:
             st.text('Регистрация не удалась. Проверьте, что все обязательные поля заполнены верно.')
 
-
+# Solution uploading
 if add_selectbox == 'Загрузка решения':
     st.header(add_selectbox)
 
@@ -91,9 +117,11 @@ if add_selectbox == 'Загрузка решения':
     password = st.text_input('Пароль')
 
     if login and password \
-    and users_database.loc[login, ].shape[0] > 0 \
-    and users_database.loc[login, 'password'] == password:
+    and login in users_database.index.values \
+    and users_database.loc[login, 'Password'] == password:
         uploaded_file = st.file_uploader('Загрузите файл с решением', accept_multiple_files=False, type=['csv'])
         if uploaded_file:
+            metric = calculate_metric(uploaded_file, gt)
+            rating = update_rating(login, metric)
             st.text(f'Полученная метрика: {0.9999}')
                    
